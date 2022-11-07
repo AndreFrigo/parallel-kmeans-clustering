39b40ba592f2216ad446eb0a96e01d8e57d2d733
AndreFrigo <andrea.boy.frigo@gmail.com>
2022-11-07 11:43:00 +0100

    Add centroids and broadcast them

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "functions.h"
 
 int main(int argc, char *argv[]){
@@ -18,32 +19,37 @@ int main(int argc, char *argv[]){
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
     MPI_Comm_size(MPI_COMM_WORLD , &n_proc);
 
-    int nrow, ncol;
+    int nrowold, nrow, ncol, k;
     float *dataMatrix=NULL;
+    float *centroids=NULL;
 
     if(my_rank == 0){
         //2 arguments are expected: first the filename of the dataset, second the number of MPI processes
-        if (argc < 3){
+        if (argc < 4){
             printf("Error! Wrong number of arguments");
             return -1;
         }
-        int nproc = atoi(argv[2]);
-
         char *filename = argv[1];
+        int nproc = atoi(argv[2]);
+        k = atoi(argv[3]);
+        
+
 
         printf("P0\n");
-        nrow = getRows(filename);
+        printf("K: %d\n", k);
+        nrowold = getRows(filename);
         ncol = getCols(filename);
         //make the number of rows divisible by the number of MPI processes used
-        if(nrow%n_proc!=0){
-            nrow += n_proc-nrow%n_proc;
+        if(nrowold%n_proc!=0){
+            nrow = nrowold + n_proc-nrowold%n_proc;
         }
 
-        //broadcast nrow and ncol
-        MPI_Bcast(&nrow, 1, MPI_INT, 0, MPI_COMM_WORLD);
-        MPI_Bcast(&ncol, 1, MPI_INT, 0, MPI_COMM_WORLD);
+        // //broadcast nrow and ncol
+        // MPI_Bcast(&nrow, 1, MPI_INT, 0, MPI_COMM_WORLD);
+        // MPI_Bcast(&ncol, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
         dataMatrix = (float *)malloc(nrow * ncol * sizeof(float));
+        
         
         readFile(filename, nrow, ncol, dataMatrix);
 
@@ -54,7 +60,8 @@ int main(int argc, char *argv[]){
     //broadcast nrow and ncol
     MPI_Bcast(&nrow, 1, MPI_INT, 0, MPI_COMM_WORLD);
     MPI_Bcast(&ncol, 1, MPI_INT, 0, MPI_COMM_WORLD);
-    printf("PROC %d\nNum rows: %d\nNum cols: %d\n", my_rank, nrow, ncol);
+    MPI_Bcast(&k, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    printf("PROC %d\nNum rows: %d\nNum cols: %d\nK: %d\n", my_rank, nrow, ncol, k);
 
     //scatter the dataMatrix
     int scatterRow=nrow/n_proc;
@@ -65,7 +72,27 @@ int main(int argc, char *argv[]){
     // printf("PROCESS %d\n", my_rank);
     // printMatrix(scatterRow, ncol, recvMatrix);
 
-    if(my_rank==0) free(dataMatrix);
+    centroids = (float *)malloc(k * ncol * sizeof(float));
+    if(my_rank==0){
+        printf("INSIDE CRITICAL SECTION, K: %d\n", k);
+        //choose randomly k centroids
+        int i;
+        srand(time(NULL));
+        for(i=0;i<k;i++){
+            int r = rand()%nrowold;
+            int c;
+            printf("Random: row: %d\n", r);
+            for(c=0;c<ncol;c++){
+                centroids[i*ncol+c] = dataMatrix[r*ncol+c];
+            }
+        }
+        // printMatrix(k, ncol, centroids);
+        free(dataMatrix);
+    }
+    MPI_Bcast(centroids, k*ncol, MPI_FLOAT, 0, MPI_COMM_WORLD);
+
+    // printf("PROC %d\n", my_rank);
+    // printMatrix(k, ncol, centroids);
 
 
     return 0;
